8159ca55321cab1b21553c0d2cff1f42aca73fb4
淋酱 <3127148186@qq.com>
2020-05-25 13:36:17 +0800

    init

---
+++ portal/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="portal-app">
+    <Tips />
+    <Layout v-if="showSubApp" />
+    <!-- 如果没有登录信息 需要 进入router-view 登录页 -->
+    <router-view v-else></router-view>
+  </div>
+</template>
+
+<script>
+import Tips from "./Layout/components/Tips";
+import Cookies from "js-cookie";
+import { changeLiveWH, pageIsClose } from "@/utils";
+import Layout from "@/Layout";
+import { mapGetters } from "vuex";
+import { getToken } from "@/utils/auth";
+export default {
+  components: { Tips, Layout },
+  computed: {
+    ...mapGetters(["showSubApp"])
+  },
+  created() {
+    // 监听浏览器关闭 清除 登录 cookie
+    pageIsClose();
+  }
+};
+</script>
